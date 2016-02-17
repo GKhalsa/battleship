4d16e15e75bed280bae7b6ea0f4294a371ecd624
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-16 20:09:46 -0700

    starting ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,17 @@
+require 'pry'
 class Ship
+  attr_reader :length, :name
+  attr_accessor :spaces
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @sunk = false
+    @spaces = Array.new(length, false)
+  end
+
+  def sunk?
+    spaces.all? {|space| space == true} ? @sunk = true : @sunk = false
+  end
+
 end
