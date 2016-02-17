b9e2678b46f70382915f251577e1425817ce63f5
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-16 20:08:57 -0700

    starting ship class

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,36 +3,35 @@ require 'minitest/pride'
 require_relative '../lib/ship.rb'
 
 class ShipTest < Minitest::Test
+
   def test_exists
-    ship = Ship.new
+    ship = Ship.new("uBoat",2)
     assert_equal Ship, ship.class
   end
 
   def test_ship_starts_with_a_length
-    ship = Ship.new
+    ship = Ship.new("uBoat",2)
     assert_equal 2, ship.length
   end
 
   def test_ship_starts_unsunk
-    ship = Ship.new
+    ship = Ship.new("uBoat",2)
     assert_equal false, ship.sunk?
   end
 
   def test_ship_starts_with_spaces
-    ship = Ship.new
-    ship.length = 2
+    ship = Ship.new("uBoat", 2)
     assert_equal [false,false], ship.spaces
   end
 
   def test_ship_is_sunk
-    ship = Ship.new
-    ship.length = 2
+    ship = Ship.new("uBoat", 2)
     ship.spaces = [true,true]
     assert ship.sunk?
   end
 
   def test_ship_has_name
-    ship = Ship.new("uBoat")
+    ship = Ship.new("uBoat", 2)
     assert_equal "uBoat", ship.name
   end
 
