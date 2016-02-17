e8b1b5a3e34bdc9a5ec2b6ef8c136e3a4812f38a
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-17 13:39:53 -0700

    save point1

--- lib/board.rb
+++ lib/board.rb
@@ -2,22 +2,52 @@ class Board
   attr_reader :grid
   def initialize
     @grid = {A:[" "," "," "," "], B:[" "," "," "," "], C:[" "," "," "," "], D:[" "," "," "," "] }
-    @ship_locations = 0
+    @ship_locations = [["A1", "A2"], ["B2", "B3"]] #maybe objects
   end
 
   def draw_grid
+    #puts =========
     grid.each do |key, spaces|
       spaces.join(" ")
+      #if I put a letter in the array, then the spaces would be 1-4 and not 0-3
     end
+    #puts =========
   end
 
-  def add_ship(ship)
+  def add_ship(input)
+    @ship_locations << #["b2", "b3"]] #input
   end
 
   def what_ships?
   end
 
-  def validate_ship
+  def can_ship_be_created?(input) #a1
+    board = [*('a'..'d'), *('1'..'4')]
+    board_check = input.chars.all? {|location| board.include?(location)}
+    ship_check = !@ship_locations.flatten.include?(input)
+    board_check && ship_check
+  end
+
+  def populate_grid
+  end
+
+  def is_a_hit?
+    @ship_locations.flatten.include?("B4")
+
+    #if it is included, search ships for "B4" and change ["B4",nil]
+    #to ["B4",true]
+    # ship1 = {'A1' => nil, 'A2' => nil}
+    # ship2 = {'B1' => nil, 'B2' => nil}
+    #change @grid[:B][4 - 1] = H   #to_sym
+    #else @grid[:B][4 - 1] = M
+    #ship_1.map! do |space, peg|
+    #  [space, peg = true] if guess == space
+    #end
+    #ship validator, scans ships for all true
+
+  end
+
+  def show_hit_on_grid
   end
 
 end
--- lib/ship.rb
+++ lib/ship.rb
@@ -1,17 +1,34 @@
 require 'pry'
 class Ship
   attr_reader :length, :name
-  attr_accessor :spaces
+  attr_accessor :occupied_spaces
 
   def initialize(name, length)
     @name = name
     @length = length
     @sunk = false
-    @spaces = Array.new(length, false)
+    @occupied_spaces = {}#Array.new(length, false)
   end
 
   def sunk?
-    spaces.all? {|space| space == true} ? @sunk = true : @sunk = false
+    # spaces.all? {|space| space == true} ? @sunk = true : @sunk = false
+    occupied_spaces.all? {|position, status| status == true }
   end
 
+  def create_ship(input)
+    input.each { |position| occupied_spaces[position] = false
+  end
+
+  def ship_validation
+      #see board class
+  end
+    #create ships
+    #"A1","A2" @spaces << [['A1',nil],['A2',nil]
+    #spaces = {'A1' => nil, 'A2' => nil}
+
 end
+
+#ship_1 = [["A1",nil], ["A2", nil]]
+#ship_1.map! do |space, peg|
+#  [space, peg = true] if guess == space
+#end
