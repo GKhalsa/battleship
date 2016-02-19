69a33893b4bef21d33cf8f984d720810ecdac143
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-19 03:42:43 -0700

    tests are good on the board

--- test/board_test.rb
+++ test/board_test.rb
@@ -12,14 +12,8 @@ class BoardTest < Minitest::Test
 
   def test_board_has_a_layout
     board = Board.new
-    layout = {:A=>["A", " ", " ", " ", " "], :B=>["B", " ", " ", " ", " "], :C=>["C", " ", " ", " ", " "], :D=>["D", " ", " ", " ", " "]}
+    layout = {:a=>["A", " ", " ", " ", " "], :b=>["B", " ", " ", " ", " "], :c=>["C", " ", " ", " ", " "], :d=>["D", " ", " ", " ", " "]}
     assert_equal layout, board.grid
-  end
-
-  def test_draw_grid
-    board = Board.new
-    layout = {:A=>["A", " ", " ", " ", " "], :B=>["B", " ", " ", " ", " "], :C=>["C", " ", " ", " ", " "], :D=>["D", " ", " ", " ", " "]}
-    assert_equal layout, board.draw_grid
   end
 
   def test_can_build_a_starting_board_for_ai
@@ -30,22 +24,70 @@ class BoardTest < Minitest::Test
 
   def test_can_human_build_a_board?
     board = Board.new
-    board.setup_human_board_with_first_ship('a1 a2')
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
     assert_equal 1, board.ship_locations.count
   end
 
   def test_cant_build_a_second_ship_if_it_overlaps_the_first
     board = Board.new
-    board.setup_human_board_with_first_ship('a1 a2')
-    board.setup_human_board_with_second_ship('a1 a2')
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
     assert_equal 1, board.ship_locations.count
   end
 
   def test_can_build_a_second_ship_if_it_doesnt_overlap_the_first
     board = Board.new
-    board.setup_human_board_with_first_ship('a1 a2')
-    board.setup_human_board_with_second_ship('a3 b1')
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    board.setup_human_board_with_ship('a3 b1', 'uBoat')
     assert_equal 2, board.ship_locations.count
+  end
+
+  def test_can_draw_grid_after_creating_ships
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    board.setup_human_board_with_ship('a3 b1', 'uBoat')
+    assert_equal nil, board.draw_grid
+  end
+
+  def test_ship_is_not_sunk_when_starting_out
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    assert_equal false, board.ship_locations[0].sunk
+  end
+
+  def test_ship_locations
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    assert_equal '*', board.grid[:a][1]
+  end
+
+  def test_if_ship_can_have_response_to_being_hit
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    assert_equal "\e[31mIt's a hit! \e[0m", board.is_a_hit?('a1')
+  end
+
+  def test_that_there_is_a_message_for_a_missed_shot
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    assert_equal "\e[34mNo hit, I repeat no hit!\e[0m", board.is_a_hit?('ab1')
+  end
+
+  def test_ship_can_be_sunk
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    board.is_a_hit?('a1')
+    assert_equal false, board.ship_locations[0].sunk
+    board.is_a_hit?('a2')
+    assert_equal true, board.ship_locations[0].sunk
+  end
+
+  meta single:true
+  def test_get_response_for_downing_a_ship
+    board = Board.new
+    board.setup_human_board_with_ship('a1 a2', 'uBoat')
+    board.is_a_hit?('a1')
+    assert_equal "\e[31mIt's a hit! \e[0muBoat is down!", board.is_a_hit?('a2')
   end
 
 end
--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -8,52 +8,46 @@ class GameplayTest < Minitest::Test
     gameplay = Gameplay.new
 
     assert_equal 2, gameplay.ai_board.ship_locations.count
-    assert_equal 0, gameplay.players_board
+    assert_equal 0, gameplay.player_board.ship_locations.count
   end
 
   def test_players_board_starts_with_zero_ships
     gameplay = Gameplay.new
 
-    assert_equal 0, gameplay.players_board
+    assert_equal 0, gameplay.player_board.ship_locations.count
   end
-    meta single:true
+
   def test_player_can_shoot_and_hit_a_ship
     gameplay = Gameplay.new
 
-    assert gameplay.player_fire_missile('b2')
+    assert gameplay.ai_board.is_a_hit?('b2')
   end
 
   def test_player_can_shoot_and_not_hit
     gameplay = Gameplay.new
 
-    refute gameplay.player_fire_missile('c1')
-  end
-
-  def test_ai_can_shoot_player
+    refute gameplay.ai_board.is_a_hit?('c1')
   end
 
   def test_when_player_hits_it_changes_board
     gameplay = Gameplay.new
-    gameplay.player_fire_missile('a1')
+    gameplay.ai_board.is_a_hit?('a1')
 
-    assert_equal '', gameplay.draw_board('a1')
+    assert_equal nil, gameplay.ai_board.draw_grid
   end
+
   def test_if_player_hits_all_locations_boat_is_sunk
     gameplay = Gameplay.new
+    gameplay.ai_board.is_a_hit?('a1')
+    gameplay.ai_board.is_a_hit?('a2')
 
-    gameplay.player_fire_missile('a1')
-    gameplay.player_fire_missile('a2')
-
-    assert_equal "uBoat is down!", gameplay.boat_response
+    assert_equal nil, gameplay.ai_board.draw_grid
   end
 
-
-  def test_if_player_downs_both_ships
+  def test_ai_fire
     gameplay = Gameplay.new
-    gameplay.player_fire_missile('a1')
-    gameplay.player_fire_missile('a2')
-    gameplay.player_fire_missile('a1')
-    gameplay.player_fire_missile('a2')
-
+    gameplay.ai_fire
+    assert gameplay.ai_missile
   end
+
 end
